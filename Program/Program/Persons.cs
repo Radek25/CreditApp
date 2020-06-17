d473414b2d1c2ddf1d46e976fac4366fa8f50a12
Radek25 <66422260+Radek25@users.noreply.github.com>
2020-06-17 16:35:27 +0200

    resolved problems in code

---
+++ Program/Program/Persons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    public class Persons
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Money { get; set; }
+        public string Date { get; set; }
+    }
+}
